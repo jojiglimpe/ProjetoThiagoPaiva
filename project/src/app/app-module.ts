f65aaf2fa66a337e04e6bceea9adb011160d4a7a
jojiglimpe <272619@unifio.edu.br>
2026-09-15 00:27:52 +0000

    Exercicio 5

--- project/src/app/app-module.ts
+++ project/src/app/app-module.ts
@@ -7,9 +7,10 @@ import { Exercicio1 } from './exercicios/exercicio1/exercicio1';
 import { Exercicio2 } from './exercicios/exercicio2/exercicio2';
 import { Exercicio3 } from './exercicios/exercicio3/exercicio3';
 import { Exercicio4 } from './exercicios/exercicio4/exercicio4';
+import { Exercicio5 } from './exercicios/exercicio5/exercicio5';
 
 @NgModule({
-  declarations: [App, Exercicio1, Exercicio2, Exercicio3, Exercicio4],
+  declarations: [App, Exercicio1, Exercicio2, Exercicio3, Exercicio4, Exercicio5],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
---
+++ project/src/app/exercicios/exercicio5/exercicio5.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio5',
+  standalone: false,
+  templateUrl: './exercicio5.html',
+  styleUrl: './exercicio5.scss',
+})
+export class Exercicio5 {
+  curtidas = 0;
+
+  curtir() {
+    this.curtidas++;
+  }
+}
