c6214419ed7ea48eb60d99ffcbb5c115b94f45b0
jojiglimpe <272619@unifio.edu.br>
2026-09-15 00:06:02 +0000

    Exercicio 2

--- project/src/app/app-module.ts
+++ project/src/app/app-module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Exercicio1 } from './exercicios/exercicio1/exercicio1';
+import { Exercicio2 } from './exercicios/exercicio2/exercicio2';
 
 @NgModule({
-  declarations: [App, Exercicio1],
+  declarations: [App, Exercicio1, Exercicio2],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
