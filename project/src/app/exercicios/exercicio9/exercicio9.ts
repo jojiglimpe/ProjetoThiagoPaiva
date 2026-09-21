d63992502b30086a9718b9733233c56b0bb9f8b3
Marks <lorenzo.signorini@hotmail.com>
2026-09-21 20:20:34 -0300

    correcao exercicio 9

--- project/src/app/exercicios/exercicio9/exercicio9.ts
+++ project/src/app/exercicios/exercicio9/exercicio9.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio9',
+  standalone: false,
+  templateUrl: './exercicio9.html',
+  styleUrl: './exercicio9.scss',
+})
 export class Exercicio9 {
   nomeProduto = 'Mouse';
   estoque = 0;
