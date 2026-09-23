5645be9de12ce36e8d62f91fa94e4222ce3ed81e
jojiglimpe <loresigmarks@gmail.com>
2026-09-23 15:37:21 +0000

    Exercicio 1

--- project/src/app/app-routing-module.ts
+++ project/src/app/app-routing-module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'livros',
+    path: 'livros',
     loadChildren: () => import('./livros/livros-module').then(m => m.LivrosModule)
+  },
+  {
+    path: 'exercicio1',
+    loadChildren: () => import('./exercicios2/exercicio1/exercicio1-module').then(m => m.Exercicio1Module)
   }
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/exercicios2/exercicio1/exercicio1-routing-module.ts
+++ project/src/app/exercicios2/exercicio1/exercicio1-routing-module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Exercicio1 } from './exercicio1';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Exercicio1
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class Exercicio1RoutingModule {}
